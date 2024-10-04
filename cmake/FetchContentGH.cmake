ff6044fd05364017d34992c63d177dc2987e819f
Jan Macheta <jan.macheta@embe.tech>
2024-10-04 02:04:58 +0200

    initial minor fixes

--- cmake/FetchContentGH.cmake
+++ cmake/FetchContentGH.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.28)
+cmake_minimum_required(VERSION 3.25)
 
 include(FetchContent)
 
@@ -9,10 +9,12 @@ function (FetchContent_DeclareGH NAME)
   cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
   set(ASSET_TYPE_DEFINED)
-
+  set(ASSET_TYPE_HEADER)
+  
   if (ARG_ASSET_NAME)
     message(DEBUG "Asset name provided: ${ARG_ASSET_NAME}")
     set(ASSET_TYPE_DEFINED TRUE)
+    set(ASSET_TYPE_HEADER "Accept: application/octet-stream")
   endif ()
 
   if (ARG_ZIPBALL)
@@ -21,7 +23,15 @@ function (FetchContent_DeclareGH NAME)
       message(FATAL_ERROR "Specifying multiple asset types is not allowed")
     endif ()
     set(ASSET_TYPE_DEFINED TRUE)
+    set(ASSET_TYPE_HEADER "Accept: application/vnd.github+json")
   endif ()
+
+
+  if ((NOT ASSET_TYPE_DEFINED) AND (NOT ARG_TARBALL))
+    message(WARNING "No asset type provided. Defaulting to tarball")
+    set(ARG_TARBALL TRUE)
+  endif ()
+
 
   if (ARG_TARBALL)
     message(DEBUG "tarball requested")
@@ -29,6 +39,8 @@ function (FetchContent_DeclareGH NAME)
       message(FATAL_ERROR "Specifying multiple asset types is not allowed")
     endif ()
     set(ASSET_TYPE_DEFINED TRUE)
+    set(ASSET_TYPE_HEADER "Accept: application/vnd.github+json")
+
   endif ()
 
   if (NOT ASSET_TYPE_DEFINED)
@@ -50,15 +62,25 @@ function (FetchContent_DeclareGH NAME)
     set(AUTH_HEADER "Authorization: Bearer ${TOKEN}")
   endif ()
 
-  string(CONCAT RELEASE_INFO_FILENAME ${ARG_REPOSITORY} "_" ${ARG_RELEASE_TAG} "_releaseinfo.json")
+  string(CONCAT RELEASE_INFO_FILENAME ${ARG_REPOSITORY} "_" ${ARG_RELEASE_TAG} ".json")
   string(REGEX REPLACE "[\\/]" "_" RELEASE_INFO_FILENAME ${RELEASE_INFO_FILENAME})
-  message(DEBUG "Release Info Filename: ${RELEASE_INFO_FILENAME}")
-  file(DOWNLOAD https://api.github.com/repos/${ARG_REPOSITORY}/releases/tags/${ARG_RELEASE_TAG}
-       ${RELEASE_INFO_FILENAME} HTTPHEADER "Accept: application/vnd.github+json"
-       HTTPHEADER "${AUTH_HEADER}" HTTPHEADER "X-GitHub-Api-Version: 2022-11-28"
-  )
 
-  file(READ ${CMAKE_CURRENT_BINARY_DIR}/${RELEASE_INFO_FILENAME} RELEASE_INFO)
+  set(RELEASE_INFO_FILEPATH ${FetchContentGH_BINARY_DIR}/info/${RELEASE_INFO_FILENAME})
+  message(DEBUG "Release Info file: ${RELEASE_INFO_FILEPATH}")
+  if (NOT IS_READABLE ${RELEASE_INFO_FILEPATH})
+    message(VERBOSE "Fetching release info")
+    file(DOWNLOAD https://api.github.com/repos/${ARG_REPOSITORY}/releases/tags/${ARG_RELEASE_TAG}
+         ${RELEASE_INFO_FILEPATH} HTTPHEADER "Accept: application/vnd.github+json"
+         HTTPHEADER "${AUTH_HEADER}" HTTPHEADER "X-GitHub-Api-Version: 2022-11-28"
+         STATUS RELEASE_INFO_STATUS
+    )
+    list(GET RELEASE_INFO_STATUS 0 RELEASE_INFO_ERROR_CODE)
+    if(NOT (RELEASE_INFO_ERROR_CODE EQUAL 0))
+      message(FATAL_ERROR "Failed to fetch release info: ${RELEASE_INFO_STATUS}")
+    endif()
+    endif ()
+
+  file(READ ${RELEASE_INFO_FILEPATH} RELEASE_INFO)
 
   set(ASSET_URL)
 
@@ -147,19 +169,29 @@ function (FetchContent_DeclareGH NAME)
     endif ()
 
     if (FOUND_ASSET_COUNT GREATER 1)
-      list(TRANSFORM FOUND_ASSET_NAME PREPEND "\n- ")
-      list(JOIN FOUND_ASSET_NAME "," ASSET_MSG)
-      string(APPEND ASSET_MSG "\n")
-      message(FATAL_ERROR "Multiple matching assets found: ${ASSET_MSG}")
-    endif ()
+      message(DEBUG "Multiple matching assets found. Looking for exact match")
 
+      list(FIND FOUND_ASSET_NAME "${ARG_ASSET_NAME}" ASSET_INDEX)
+      if (ASSET_INDEX EQUAL -1)
+        list(TRANSFORM FOUND_ASSET_NAME PREPEND "\n- ")
+        list(JOIN FOUND_ASSET_NAME "," ASSET_MSG)
+        string(APPEND ASSET_MSG "\n")
+        message(FATAL_ERROR "Multiple assets found with no exact match: ${ASSET_MSG}")
+      else()
+      message(DEBUG "Exact match found at index ${ASSET_INDEX}")
+      list(GET  FOUND_ASSET_NAME ${ASSET_INDEX} FOUND_ASSET_NAME)
+      list(GET  FOUND_ASSET_URL ${ASSET_INDEX} FOUND_ASSET_URL)
+
+      endif ()
+    endif()
     set(ASSET_URL ${FOUND_ASSET_URL})
   endif ()
 
   message(VERBOSE "Asset URL: ${ASSET_URL}")
 
   FetchContent_Declare(
-    ${NAME} HTTP_HEADER "Accept: application/octet-stream" "${AUTH_HEADER}" URL ${ASSET_URL}
+    ${NAME} HTTP_HEADER "${ASSET_TYPE_HEADER}" "${AUTH_HEADER}" URL ${ASSET_URL}
+    ${ARG_UNPARSED_ARGUMENTS}
   )
 
 endfunction ()
